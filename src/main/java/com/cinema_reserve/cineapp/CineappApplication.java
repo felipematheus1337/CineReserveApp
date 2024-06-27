a61777e27ad721188b7eabf34939cc4985a5b0d1
Felipe <lipehbr@gmail.com>
2024-06-27 11:39:38 -0300

    Projeto criado

---
+++ src/main/java/com/cinema_reserve/cineapp/CineappApplication.java
@@ -0,0 +1,13 @@
+package com.cinema_reserve.cineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinema_reserve/cineapp/CineappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema_reserve.cineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
